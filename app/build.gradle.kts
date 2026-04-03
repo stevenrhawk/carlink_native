c58039e40f2b69a18625ff768b006bc8c772bcc9
stevenrhawk <stevenrhawk@gmail.com>
2026-04-02 22:24:56 -0400

    namespace

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 //###############################################
 
     defaultConfig {
-        applicationId = "zeno.carlink"
+        applicationId = "com.afterhourstech.cpaaplayer"
         minSdk = 32
         targetSdk = 36
         versionCode = 103
